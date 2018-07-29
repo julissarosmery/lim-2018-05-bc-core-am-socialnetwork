dd237bcc0e2c7762908c6c47353e4a680602abea
julissa <rosmery0239@gmail.com>
2018-07-29 18:45:29 -0500

    login exitoso

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-logInOnRegister=()=>{
+window.onload =()=>{
+    firebase.auth().onAuthStateChanged((user)=>{
+        if(user){
+            //si resta,mps logeados 
+            loggedOut.style.display = "none";
+            loggedIn.style.display ="block";
+            console.log("User >"+ JSON.stringify(user));
+        }else{
+            //si no estamos logeados
+            loggedOut.style.display ="block";
+            loggedIn.style.display ="none";
+            
+
+            
+        }
+    });
+}
+
+window.register =() => {
     const emailValue = email.value;
     const passwordValue = password.value;
     firebase.auth().createUserWithEmailAndPassword(emailValue,passwordValue)
-    .them(()=>{
+    .then(() =>{
         console.log("usuario registrado");
 
     })
@@ -10,5 +28,18 @@ logInOnRegister=()=>{
         console.log("error de firebase >" +error.code);
         console.log("errorfirebase,mensaje >" +error.message);
     });
+}
+window.login=()=>{
+    const emailValue = email.value;
+    const passwordValue = password.value;
+    firebase.auth().signInWithEmailAndPassword(emailValue,passwordValue)
+    .then(()=>{
+        console.log("usuario con login exitoso")
+    })
+    .catch((error)=>{
+        console.log("error de firebase >"+ error.code);
+        console.log("error de firebase ,mensaje >" +error.message);
+
+    })
 }
 
